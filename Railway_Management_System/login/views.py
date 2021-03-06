b0e65b0d48b28d29ce23d7adf93feb05088c3bb5
morkerdeep <75012609+morkerdeep@users.noreply.github.com>
2021-03-07 01:46:27 +0530

    Added login and register feature

--- Railway_Management_System/login/views.py
+++ Railway_Management_System/login/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -6,4 +9,18 @@ def index(request):
     return render(request, "index.html")
 
 def login(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        username = request.POST['uname']
+        password = request.POST['pswd']
+
+        user = auth.authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return render(request, "hello.html")
+        else:
+            messages.error(request, "Invalide Credential")
+            return render(request, "login.html")
+    else:
+        return render(request, "login.html")
+
+    
--- Railway_Management_System/register/views.py
+++ Railway_Management_System/register/views.py
@@ -1,5 +1,27 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def register(request):
-    return render(request, "register.html")
+    if request.method == "POST":
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        username = request.POST['uname']
+        password = request.POST['pwd']
+        repassword = request.POST['rpwd']
+        
+        if ( fname == "" or lname == "" or username == "" or email == "" or password == ""):
+            messages.error(request, "All field must be filled")
+            return render(request, "register.html")
+        elif ( password != repassword ):
+            messages.error(request, "Re-entered password must be same as entered password")
+            return render(request, "register.html")
+        else:
+            # user = User.objects.create_user(username = username, password = password, email = email, first_name = fname, last_name = lname)
+            # user.save()
+            messages.error(request, "You have successfully registered yourself. Now you can log in :)")
+            return render(request, "login.html")
+    else:
+        return render(request, "register.html")
